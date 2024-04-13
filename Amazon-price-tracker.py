4b5a660a0444e6140db1d6379b3b1766972ef649
Tobi <155114173+tobilobasalawu@users.noreply.github.com>
2024-04-13 13:53:31 +0100

    Update Amazon-price-tracker.py

--- Amazon-price-tracker.py
+++ Amazon-price-tracker.py
@@ -33,7 +33,7 @@ def send_mail():
     server.starttls()
     server.ehlo()
     
-    server.login('tobisal.dev@gmail.com', 'gzqd wgrm gbtw myfd')
+    server.login('username@gmail.com', 'password')
     
     subject = 'Price fell down'
     body = 'check the amazon link https://www.amazon.co.uk/Veno-Scorp-Gaming-Bundle-Set/dp/B0BDMV65L3/ref=sr_1_4?crid=26JIKCFTS05W2&dib=eyJ2IjoiMSJ9.n-kGti1DZUbAdS_Gc8KlJ2Mtii3nTeb3eK_dScGen4bTYc_VGqMUKl_7sxA0ijW_UQD8TsGkj2RKJ3hon_coH_Q-iuUIpij4eaAivMVsvg_W5ovmZZKaR_MEJkDpg0mLWgs-Q-PcfMH_XrkWxWAItnfcITEJ25NuHXjsEu-djy42eCW6hfI5L6ylOXf66u3FSF6OiuYG2unpDO5vZQxhHl6OWFszI8Mm2VvTWfeNnaA.YH9ZA_dqprXbzhY5Uw9PofRjTWs68GNbdYFcj2zPLWc&dib_tag=se&keywords=pc+coding&qid=1712952315&sprefix=pc+coding%2Caps%2C245&sr=8-4'
@@ -41,8 +41,8 @@ def send_mail():
     msg = f"Subject : {subject}\n\n{body}"
     
     server.sendmail(
-        'tobisal.dev@gmail.com',
-        'oluwatobilobasalawu1234@gmail.com',
+        'sender@gmail.com',
+        'recipient@gmail.com',
         msg
         )
     print('Hey EMAIL has been sent')
